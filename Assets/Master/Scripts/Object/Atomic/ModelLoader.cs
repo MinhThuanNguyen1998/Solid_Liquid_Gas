e6f0dd3f8fe7b3652c1a0b17cc6cbce2ac54184a
thuannm <thuannm.ot@gmail.com>
2025-12-05 17:30:55 +0700

    - Update : clear useless lines

--- Assets/Master/Scripts/Object/Atomic/ModelLoader.cs
+++ Assets/Master/Scripts/Object/Atomic/ModelLoader.cs
@@ -9,9 +9,6 @@ public class ModelLoader : MonoBehaviour
     [SerializeField] private GameObject m_GasModel;
     [SerializeField] private ElementChecker m_ElementChecker;
     private GameObject m_CurrentStateModel;
-    private GameObject m_CurrentElementModel;
-    private Dictionary<string, GameObject> elementPrefabCache = new();
-
     private void OnEnable()
     {
         InitializeDefault();
@@ -19,28 +16,24 @@ public class ModelLoader : MonoBehaviour
     private void InitializeDefault()
     {
         LoadStateModel(Config.Solid, loadDefaultElement: false);
-        LoadElementModel(Config.DefaultState_Solid);
     }
     public void LoadStateModel(string state, bool loadDefaultElement = true)
     {
         ClearCurrentStateModel();
-        ClearCurrentElementModel();
         GameObject prefabToLoad = null;
         switch (state)
         {
             case var _ when state == Config.Solid:
                 prefabToLoad = m_SolidModel;
-                if (loadDefaultElement) LoadElementModel(Config.DefaultState_Solid);
+          
                 break;
 
             case var _ when state == Config.Liquid:
                 prefabToLoad = m_LiquidModel;
-                if (loadDefaultElement) LoadElementModel(Config.DefaultState_Liquid);
+            
                 break;
-
             case var _ when state == Config.Gas:
                 prefabToLoad = m_GasModel;
-                if (loadDefaultElement) LoadElementModel(Config.DefaultState_Gas);
                 break;
         }
         if (prefabToLoad != null)
@@ -49,28 +42,6 @@ public class ModelLoader : MonoBehaviour
             m_CurrentStateModel.transform.parent = transform;
         }
     }
-    public void LoadElementModel(string elementName)
-    {
-        //Debug.Log("Atomic:" + elementName); 
-        ClearCurrentElementModel();
-        if (!elementPrefabCache.TryGetValue(elementName, out GameObject prefabToLoad))
-        {
-            prefabToLoad = Resources.Load<GameObject>($"Models/{elementName}");
-            if (prefabToLoad != null)
-            {
-                elementPrefabCache[elementName] = prefabToLoad; // Cache prefab
-            }
-            else
-            {
-                Debug.LogWarning($"[ModelLoader] Prefab not found for element: {elementName}. Expected path: Resources/Models/{elementName}.prefab");
-                return;
-            }
-        }
-        m_CurrentElementModel = Instantiate(prefabToLoad);
-        m_CurrentElementModel.transform.parent = transform;
-        m_ElementChecker?.OnCheckElement(elementName);
-    }
-  
     private void ClearModel(ref GameObject model)
     {
         if (model != null)
@@ -80,6 +51,4 @@ public class ModelLoader : MonoBehaviour
         }
     }
     private void ClearCurrentStateModel() => ClearModel(ref m_CurrentStateModel);
-    private void ClearCurrentElementModel() => ClearModel(ref m_CurrentElementModel);
-
 }
--- Assets/Master/Scripts/View/LabMode.cs
+++ Assets/Master/Scripts/View/LabMode.cs
@@ -10,42 +10,18 @@ public class LabMode : MonoBehaviour
 {
     public static event Action<int> OnGotoState;
     [SerializeField] private TMP_Dropdown m_DropDownStateOfMatter;
-    [SerializeField] private TMP_Dropdown m_DropDownAtomic;
     [SerializeField] private ModelLoader m_ModelLoader;
     private void OnEnable() 
     {
-        SetDefaultState();
-        m_DropDownStateOfMatter.onValueChanged.AddListener(OnStateChanged);
-        m_DropDownAtomic.onValueChanged.AddListener(OnElementChanged);
+        m_DropDownStateOfMatter.onValueChanged.AddListener(OnStateChanged);  
     }
     private void OnDisable() 
     {
         m_DropDownStateOfMatter.onValueChanged.RemoveListener(OnStateChanged);
-        m_DropDownAtomic.onValueChanged.RemoveListener (OnElementChanged);
     } 
     private void OnStateChanged(int index)
     {
         string selectedState = m_DropDownStateOfMatter.options[index].text;
-        UpdateElementDropdown(selectedState);
         m_ModelLoader?.LoadStateModel(selectedState);
-    }
-    private void OnElementChanged(int index)
-    {
-        string selectedElement = m_DropDownAtomic.options[index].text;
-        m_ModelLoader?.LoadElementModel(selectedElement);
-    }
-    private void UpdateElementDropdown(string state)
-    {
-        m_DropDownAtomic.ClearOptions();
-        //Debug.Log("SelectedState:" + state);
-        if (Config.ElementsByState.ContainsKey(state))
-        {
-            m_DropDownAtomic.AddOptions(Config.ElementsByState[state]);
-        }
-        else Debug.LogWarning($"[LabMode] No elements found for state: '{state}'. Please check Config.ElementsByState.");
-    }
-    private void SetDefaultState()
-    {
-        UpdateElementDropdown(Config.Solid);
     }
 }
