e0a75c53c64e4dc32bdf053d6ec772fec33b0f02
thuannm <thuannm.ot@gmail.com>
2025-12-11 11:55:47 +0700

    - Update : Gas Phase transition

--- Assets/Master/Scripts/Object/BottleTrigger.cs
+++ Assets/Master/Scripts/Object/BottleTrigger.cs
@@ -9,6 +9,8 @@ public class BottleTrigger : BaseTrigger
     [SerializeField] private StepGas m_StepGas;
     [SerializeField] GameObject m_ParentBottle;
     [SerializeField] private Transform m_AnchorPoint;
+    [SerializeField] private GameObject m_GasStructureObject;
+    [SerializeField] private GameObject m_LiquidStructureObject;
 
     [Header("Material")]
     [SerializeField] private Material m_OriginMaterial;
@@ -18,6 +20,8 @@ public class BottleTrigger : BaseTrigger
 
     private float m_WaitingTimeToStartCoroutine = 5f;
     private float m_Duration = 10f;
+
+    private void Awake() => SetStructureState(false, false);
     protected override void OnEnter(Collider other)
     {
         if (other.CompareTag("Lid"))
@@ -38,9 +42,13 @@ public class BottleTrigger : BaseTrigger
     {
         yield return new WaitForSeconds(m_WaitingTimeToStartCoroutine);
         float halfDuration = duration / 2f;
-        yield return StartCoroutine(BlendMaterial(start, mid, duration));
-        yield return StartCoroutine(BlendMaterial(mid, end, duration));
         MagnifyingManager.Instance.ActiveMagnifyingObject(true);
+        SetStructureState(true, false);
+        yield return StartCoroutine(BlendMaterial(start, mid, duration));
+        SetStructureState(false, true);
+        yield return StartCoroutine(BlendMaterial(mid, end, duration));
+        
+
     }
     private IEnumerator BlendMaterial(Material from, Material to, float time)
     {
@@ -55,5 +63,9 @@ public class BottleTrigger : BaseTrigger
         // Gán mid Material sau Phase 1
         m_BottleRenderer.material = to;
     }
-
+    private void SetStructureState(bool solid, bool liquid)
+    {
+        m_GasStructureObject.SetActive(solid);
+        m_LiquidStructureObject.SetActive(liquid);
+    }
 }
