80cf3304204d265191ff3970a5f03ee332be0628
thuannm <thuannm.ot@gmail.com>
2025-12-12 16:52:18 +0700

    - Update : random position for gas structure

--- Assets/Master/Scripts/Object/StructureMovingController.cs
+++ Assets/Master/Scripts/Object/StructureMovingController.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 
 public class StructureMovingController : MonoBehaviour
 {
+    [Header("Common Settings")]
     [SerializeField] private float m_Amplitude = 0.01f;
     [SerializeField] private float m_Speed = 2f;
+
+    [Header("Gas Settings")]
+    [SerializeField] private bool IsGas = false;
+    [SerializeField] private float GasRadius = 0.2f;
+    [SerializeField] private float GasSpeed = 1f;
 
     private List<Transform> m_Children = new List<Transform>();
     private Dictionary<Transform, Vector3> m_OriginalPos = new Dictionary<Transform, Vector3>();
@@ -19,14 +25,27 @@ public class StructureMovingController : MonoBehaviour
             m_OriginalPos.Add(child, child.localPosition);
         }
     }
+
     void Update()
     {
         float t = Time.time * m_Speed;
-
         foreach (Transform child in m_Children)
         {
-            float offset = Mathf.Sin(t + child.GetInstanceID()) * m_Amplitude;
-            child.localPosition = m_OriginalPos[child] + new Vector3(offset, offset, 0);
+            if (!IsGas)
+            {
+                float offset = Mathf.Sin(t + child.GetInstanceID()) * m_Amplitude;
+                child.localPosition = m_OriginalPos[child] + new Vector3(offset, offset, 0);
+            }
+            else
+            {
+                float gasT = Time.time * GasSpeed;
+                float id = child.GetInstanceID() * 0.1f;
+
+                float x = Mathf.PerlinNoise(gasT + id, 0) - 0.5f;
+                float y = Mathf.PerlinNoise(0, gasT + id) - 0.5f;
+                Vector3 offset = new Vector3(x, y, 0) * GasRadius;
+                child.localPosition = m_OriginalPos[child] + offset;
+            }
         }
     }
 }
